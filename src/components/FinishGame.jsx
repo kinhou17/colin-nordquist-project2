b060b94723ece73d35f971d4e701e67fbb434567
Colin Nordquist <ckhnordquist@gmail.com>
2022-04-10 21:07:35 -0700

    5 man wordle working

--- src/components/FinishGame.jsx
+++ src/components/FinishGame.jsx
@@ -1,5 +1,6 @@
 import React, {useContext} from "react";
 import {WordleContext} from "./App";
+import './FinishGame.css'
 
 export default function GameOver() {
     const {gameState, currGuess, winningWord} = useContext(WordleContext);
@@ -11,11 +12,17 @@ export default function GameOver() {
         winLose = "YOU FAILED";
     }
 
+    function refreshPage() {
+        window.location.reload(false);
+      }
+
     return (
         <div className="Game Over!">
             <h2>{winLose}</h2>
             <h2>Correct: {winningWord} </h2>
             {gameState.playerWon && (<h2>You guessed in {currGuess.guess + 1} attempts</h2>)}
+            <button onClick={refreshPage}>Click to play again!</button>
         </div>
     )
 }
+
